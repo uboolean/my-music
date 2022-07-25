141af470a29c2da2e234c3e83ca673ee280cb229
B in <me_bin@outlook.com>
2022-07-25 18:09:32 +0800

    修改接口文件说明

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
 export default{
+
+	//接口具体作用请移步至接口提供的官方文档
+
 	Music163:{
-		// SearchUrl:'http://10.10.1.134:3000/cloudsearch',
-		// GetDonwoladUrl:'http://10.10.1.134:3000/song/url',
-		// GetSongDetail:'http://10.10.1.134:3000/song/detail'
+		SearchUrl:'http://10.10.1.134:3000/cloudsearch', 
+		GetDonwoladUrl:'http://10.10.1.134:3000/song/url',
+		GetSongDetail:'http://10.10.1.134:3000/song/detail'
 	},
 	MusicMigu:{
 		SearchUrl:'http://10.10.1.134:3400/search',
