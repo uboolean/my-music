317e053f20e5be70bbefefef40ff8fee5b23c18b
Boolean <91360663+uboolean@users.noreply.github.com>
2022-07-26 10:11:20 +0800

    nothing

--- config/config.js
+++ config/config.js
@@ -1,13 +1,4 @@
 export default{
-<<<<<<< HEAD
-
-	//接口具体作用请移步至接口提供的官方文档
-
-	Music163:{
-		SearchUrl:'http://10.10.1.134:3000/cloudsearch', 
-		GetDonwoladUrl:'http://10.10.1.134:3000/song/url',
-		GetSongDetail:'http://10.10.1.134:3000/song/detail'
-=======
 	//修改接口请参考官方接口文档
 	
 	//网易云
@@ -15,7 +6,6 @@ export default{
 		SearchUrl:'http://10.10.1.134:3000/cloudsearch', //音乐搜索接口
 		GetDonwoladUrl:'http://10.10.1.134:3000/song/url', //音乐下载链接获取接口
 		GetSongDetail:'http://10.10.1.134:3000/song/detail' //获取音乐详情接口
->>>>>>> a0352138f495b044be99d44769a6eeaaff423eac
 	},
 	//咪咕音乐
 	MusicMigu:{
